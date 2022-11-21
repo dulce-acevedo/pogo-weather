2647c35d11d1e77eb7925b911e81f6fc34a40c28
dulce-acevedo <n10370021@qut.edu.au>
2022-11-22 08:51:05 +1000

    Added tailwind and daisy

--- app/ClientApp/src/components/Home.js
+++ app/ClientApp/src/components/Home.js
@@ -6,8 +6,10 @@ export class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <h1 className="  bg-green-500
+ text-2xl font-bold underline">Hello, world!</h1>
         <p>Welcome to your new single-page application, built with:</p>
+        <button class="btn rounded-full">One</button>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
