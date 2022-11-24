e4a34387a67c5f15e58a9e68e334caee81d84be4
dulce-acevedo <n10370021@qut.edu.au>
2022-11-24 23:19:17 +1000

    Set a theme with tailwind

--- app/ClientApp/tailwind.config.js
+++ app/ClientApp/tailwind.config.js
@@ -4,10 +4,37 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins']
+      }
+    }
   },
   daisyui: {
-    themes: ["valentine"],
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#e8414f",
+        
+"secondary": "#f3f4f6",
+        
+"accent": "#e11d48",
+        
+"neutral": "#e11d48",
+        
+"base-100": "#e8414f",
+        
+"info": "#e11d48",
+        
+"success": "#e11d48",
+        
+"warning": "#e11d48",
+        
+"error": "#e11d48",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
   
